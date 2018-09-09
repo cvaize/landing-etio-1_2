fa82f785b4f77a1c8e5f4f9d8c22e4eea6cb6c4e
cvaize <cvaize@gmail.com>
2018-09-09 20:25:23 +0500

    Finall

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -12,34 +12,29 @@
         <link href="https://fonts.googleapis.com/css?family=Nunito+Sans:400,600,700,800,900" rel="stylesheet">
         <link rel="stylesheet" href="/css/app.css">
     </head>
-    <body >
+    <body>
     @include("layouts.navbar")
-
-    @if(true)
-        @include("screens.1_main")
-        @include("screens.2_1_screen")
-        @include("screens.3_1_screen")
-        @include("screens.3_screen")
-        @include("screens.4_1_screen")
-        @include("screens.5_1_screen")
-        @include("screens.6_screen")
-        @include("screens.7_1_screen")
-        <div class="screen--8__pc">
-            @include("screens.7_screen")
-            @include("screens.8_screen")
-        </div>
-        @include("screens.9_screen")
-        @include("screens.10_screen")
-        @include("screens.11_screen")
-        @include("screens.12_screen")
-        @include("screens.13_screen")
-        @include("screens.14_screen")
-        @include("screens.15_screen")
-        @include("screens.16_screen")
-    @endif
+    @include("screens.1_main")
+    @include("screens.2_1_screen")
+    @include("screens.3_1_screen")
+    @include("screens.3_screen")
+    @include("screens.4_1_screen")
+    @include("screens.5_1_screen")
+    @include("screens.6_screen")
+    @include("screens.7_1_screen")
+    <div class="screen--8__pc">
+        @include("screens.7_screen")
+        @include("screens.8_screen")
+    </div>
+    @include("screens.9_screen")
+    @include("screens.10_screen")
+    @include("screens.11_screen")
+    @include("screens.12_screen")
+    @include("screens.13_screen")
+    @include("screens.14_screen")
+    @include("screens.15_screen")
+    @include("screens.16_screen")
     @include("screens.17_map")
-    <div style="height: 50rem; background: gray;"></div>
-
     @include("layouts.footer")
     <script src="/js/app.js"></script>
     </body>
